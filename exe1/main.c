92cc5c24437316941199f53a2c8387fd642e376e
viniciusgomes2005 <vinixviii@gmail.com>
2025-02-19 14:15:29 -0300

    corrigindo o 1

--- exe1/main.c
+++ exe1/main.c
@@ -5,19 +5,23 @@
 const int BTN_PIN_R = 28;
 
 volatile int flag;
+volatile int fall;
 
 void btn_callback(uint gpio, uint32_t events) {
+  flag = 1;
   if (events == 0x4) { // fall edge
-    flag = 1;
+    fall = 1;
   } else if (events == 0x8) { // rise edge
-    flag = 0;
+    fall = 0;
   }
 }
 void update(){
-  if(flag ==1) {
-    printf("fall \n");
-  }else{
-    printf("rise \n");
+  if(flag){
+    if(fall ==1) {
+      printf("fall \n");
+    }else{
+      printf("rise \n");
+    }
   }
 }
 int main() {
@@ -32,7 +36,6 @@ int main() {
 
   while (true) {
     if(flag){
-      printf(flag);
       update();
     }
   }
