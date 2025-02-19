134a4181e784fbcc79e4e681b005ce7c56469357
viniciusgomes2005 <vinixviii@gmail.com>
2025-02-19 14:10:53 -0300

    .

--- exe1/main.c
+++ exe1/main.c
@@ -4,14 +4,22 @@
 
 const int BTN_PIN_R = 28;
 
+volatile int flag;
+
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
-    printf("fall \n");
+    flag = 1;
   } else if (events == 0x8) { // rise edge
+    flag = 0;
+  }
+}
+void update(){
+  if(flag ==1) {
+    printf("fall \n");
+  }else{
     printf("rise \n");
   }
 }
-
 int main() {
   stdio_init_all();
 
@@ -23,5 +31,9 @@ int main() {
       BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
   while (true) {
+    if(flag){
+      printf(flag);
+      update();
+    }
   }
 }
